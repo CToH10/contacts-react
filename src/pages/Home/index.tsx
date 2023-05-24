ea853fd7440ca546fcc40b83657876f8a4bd16f6
CToH10 <luis.ebn98@gmail.com>
2023-05-24 17:13:07 -0300

    feat: add contactCard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,12 @@
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Container,
+} from "@mui/material";
+import { Mail, PhoneAndroid } from "@mui/icons-material";
 import { UserContext } from "../../providers/user.provider";
 import { useContext, useEffect } from "react";
 
@@ -10,12 +18,67 @@ export const HomePage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const since = (date: string) => {
+    const newDate = new Date(date);
+    const year = newDate.getFullYear();
+    const month = newDate.getMonth();
+
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    return `Since ${monthNames[month]}/${year}`;
+  };
+
   return (
-    <>
+    <Container>
       <Typography variant="h1">Welcome, to the mato</Typography>
       {foundContacts.map((contact) => (
-        <li key={contact.id}>{contact.fullName}</li>
+        <Card
+          sx={{ maxWidth: 255, minWidth: 120 }}
+          key={contact.id}
+          id={contact.id}
+        >
+          <CardContent>
+            <Typography gutterBottom variant="h4">
+              {contact.fullName}
+            </Typography>
+            <Typography>{since(contact.registered)}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              type="button"
+              size="small"
+              rel="noopener nonreferrer"
+              href={`mailto:${contact.email}`}
+              endIcon={<Mail />}
+              aria-label="Button to send email to contact"
+            >
+              Email
+            </Button>
+            <Button
+              type="button"
+              size="small"
+              rel="noopener nonreferrer"
+              endIcon={<PhoneAndroid />}
+              aria-label="Button showing contact phone"
+            >
+              {contact.phone}
+            </Button>
+          </CardActions>
+        </Card>
       ))}
-    </>
+    </Container>
   );
 };
