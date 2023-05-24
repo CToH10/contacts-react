440b6d83906de33be8ab9820969e5cea054a476e
CToH10 <luis.ebn98@gmail.com>
2023-05-24 20:27:28 -0300

    message: reason no password

--- src/components/Forms/Edit/edit.validators.ts
+++ src/components/Forms/Edit/edit.validators.ts
@@ -8,3 +8,5 @@ export const editProfileSchema = z.object({
 
 export type EditProfileData = z.infer<typeof editProfileSchema>;
 export type EditContactData = z.infer<typeof editProfileSchema>;
+
+// For now, editing password would be too strenuous. I'd have to find a way to compare the password with de DB, to ensure no accidental changes would be made. The API never provides the password, so comparing them would be difficult
